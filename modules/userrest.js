76ddab6673f9679623fd6badef3924146be1dea5
Rareș-Petru Opăriuc <34307350+raresopariuc@users.noreply.github.com>
2018-03-28 13:40:51 +0300

    Updated userrest.js

--- modules/userrest.js
+++ modules/userrest.js
@@ -1,29 +1,28 @@
-module.exports = function(app, auth, mongoose){
-	var User = mongoose.model('UserTerraDev');
+module.exports = function(app, auth, mongoose) {
+	var User = mongoose.model ('UserTerraDev');
 
-	app.get('/findAllUsers', function(req, res){
-	User.find({})
-		.select('-password')
-		.select('-sesstoken')
-		.select('-professional')
-		.select('-hasAccount')
-		.select('-isConfirmed')
-		
-		.catch(err =>{
-			console.log(errus);
-		})
-		.then(result =>{
-			res.send(result);
-		});
+	app.get ('/findAllUsers', function(req, res) {
+		User.find ({})
+			.select ('-password')
+			.select ('-sesstoken')
+			.select ('-professional')
+			.select ('-hasAccount')
+			.select ('-isConfirmed')
+			.catch (err => {
+				console.log (err);
+			})
+			.then (result => {
+				res.send(result);
+			});
 	});
 
-	app.post('/createUser', function(req, res){
-		var userTemp = new User(req.body);
+	app.post ('/createUser', function(req, res) {
+		var userTemp = new User (req.body);
 		userTemp.save();
 	});
 
 	app.get('/findUserById/:idUser', function(req, res){
-		User.find({_id: req.params.idUser})
+		User.find({"_id": req.params.idUser})
 			.catch(err =>{
 				console.log(err);
 		})
@@ -42,4 +41,33 @@ module.exports = function(app, auth, mongoose){
 		});
 	});
 
+	app.put ('/updateUser', function(req, res) {
+		User.updateOne (req.body.find, req.body.replace)
+			.catch (err => {
+				console.log (err);
+			})
+			.then (() => {
+				console.log ('success');
+			});
+	});
+
+	app.delete ('/deleteUserById/:id', function(req, res) {
+		User.deleteOne ({"_id": req.params.id})
+			.catch (err => {
+				console.log (err);
+			})
+			.then (() => {
+				console.log ('success');
+			});
+	});
+
+	app.delete ('/deleteUserByUsername/:username', function(req, res) {
+		User.deleteOne ({username: req.params.username})
+			.catch (err => {
+				console.log (err);
+			})
+			.then (() => {
+				console.log ('success');
+			});
+	});
 };
